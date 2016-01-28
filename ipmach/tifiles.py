6efb125901b6cd677dabbd79de0e0cbfcf459048
Pengfei Li <ldsoar1990@gmail.com>
2016-01-28 11:13:37 -0500

    Update IPMach.py.

--- ipmach/tifiles.py
+++ ipmach/tifiles.py
@@ -1,5 +1,5 @@
 # This module for Generating the TI files
-
+from __future__ import print_function
 import os
 
 Mass = {'H':   1.008,  'C':  12.01,  'N':  14.01,  'O':  16.00,  'S':  32.06,
@@ -21,98 +21,139 @@ Mass = {'H':   1.008,  'C':  12.01,  'N':  14.01,  'O':  16.00,  'S':  32.06,
 def write_frcmod(ion, fname):
     mass = Mass[ion.element]
     frcmodf = open(fname, 'w')
-    print >> frcmodf, "# remark goes here"
-    print >> frcmodf, "MASS"
-    print >> frcmodf, "%s  %5.2f" %(ion.attype, mass)
-    print >> frcmodf, " "
-    print >> frcmodf, "BOND"
-    print >> frcmodf, " "
-    print >> frcmodf, "ANGL"
-    print >> frcmodf, " "
-    print >> frcmodf, "DIHE"
-    print >> frcmodf, " "
-    print >> frcmodf, "NONB"
-    print >> frcmodf, "%s    %5.3f    %10.8f" %(ion.attype, ion.rmin, ion.ep)
-    print >> frcmodf, " "
+    print("# remark goes here", file=frcmodf)
+    print("MASS", file=frcmodf)
+    print("%s  %5.2f" %(ion.attype, mass), file=frcmodf)
+    print(" ", file=frcmodf)
+    print("BOND", file=frcmodf)
+    print(" ", file=frcmodf)
+    print("ANGL", file=frcmodf)
+    print(" ", file=frcmodf)
+    print("DIHE", file=frcmodf)
+    print(" ", file=frcmodf)
+    print("NONB", file=frcmodf)
+    print("%s    %5.3f    %10.8f" %(ion.attype, ion.rmin, ion.ep), file=frcmodf)
+    print(" ", file=frcmodf)
     frcmodf.close()
 
 def write_cmd(ion):
     cmdf = open(ion.resname+'.cmd', 'w')
-    print >> cmdf, "i = createAtom %s %s %3.1f" %(ion.atname, ion.attype, ion.charge)
-    print >> cmdf, "set i element \"%s\"" %ion.element
-    print >> cmdf, "set i position { 0 0 0 }"
-    print >> cmdf, "r = createResidue %s" %ion.resname
-    print >> cmdf, "add r i"
-    print >> cmdf, "%s = createUnit %s" %(ion.resname, ion.resname)
-    print >> cmdf, "add %s r" %ion.resname
-    print >> cmdf, "saveOff %s ./%s.lib" %(ion.resname, ion.resname)
-    print >> cmdf, "quit"
+    print("i = createAtom %s %s %3.1f" %(ion.atname, ion.attype, ion.charge), file=cmdf)
+    print("set i element \"%s\"" %ion.element, file=cmdf)
+    print("set i position { 0 0 0 }", file=cmdf)
+    print("r = createResidue %s" %ion.resname, file=cmdf)
+    print("add r i", file=cmdf)
+    print("%s = createUnit %s" %(ion.resname, ion.resname), file=cmdf)
+    print("add %s r" %ion.resname, file=cmdf)
+    print("saveOff %s ./%s.lib" %(ion.resname, ion.resname), file=cmdf)
+    print("quit", file=cmdf)
     cmdf.close()
 
-def write_leapin(ion0, ion1):
+def write_leapin(ion0, ion1, watermodel):
+
     leapf = open('tleap.in', 'w')
-    print >> leapf, "source leaprc.ff14SB"
-    print >> leapf, "loadoff %s.lib" %ion0.resname
-    print >> leapf, "ion = loadpdb ION.pdb"
-    print >> leapf, "solvatebox ion OPCBOX 13"
-    print >> leapf, "loadamberparams frcmod.opc"
-    print >> leapf, "loadamberparams %s.frcmod" %ion0.attype  #VDW=0
-    print >> leapf, "loadamberparams %s.frcmod" %ion1.attype  #VDW!=0
-    print >> leapf, "ion2 = copy ion"
-    print >> leapf, "remove ion2 ion2.2"
-    #For sander: with M0 without VDW
-    print >> leapf, "saveamberparm ion2 %s_wat_szero.prmtop %s_wat_szero.inpcrd" %(ion0.atname, ion0.atname)
-    #For pmemd: with Na has VDW and M0 doesn't have VDW
-    print >> leapf, "saveamberparm ion %s_wat_pti.prmtop %s_wat_pti.inpcrd" %(ion0.element, ion0.element)
-    print >> leapf, "loadamberparams %s_vdw.frcmod" %ion0.attype #VDW != 0
-    #For pmemd: with Na and M0 all have VDW
-    print >> leapf, "saveamberparm ion %s_wat_pvdw.prmtop %s_wat_pvdw.inpcrd" %(ion0.element, ion0.element)
-    print >> leapf, "remove ion ion.1"
-    #For md and sander with Na+ has charge and VDW
-    print >> leapf, "saveamberparm ion %s_wat_md.prmtop %s_wat_md.inpcrd" %(ion0.element, ion0.element)
-    #For sander, with M0 has VDW
-    print >> leapf, "saveamberparm ion2 %s_wat_svdw.prmtop %s_wat_svdw.inpcrd" %(ion0.atname, ion0.atname)
-    print >> leapf, "quit"
+
+    print("source leaprc.ff14SB", file=leapf)
+
+    print("loadoff %s.lib" %ion0.resname, file=leapf)
+
+    # 1st is the metal ion without VDW or CHG, 2nd is the one with VDW and CHG
+    print("ion = loadpdb ION.pdb", file=leapf)
+
+    if watermodel == 'OPC':
+        print("solvatebox ion OPCBOX 13", file=leapf)
+        print("loadamberparams frcmod.opc", file=leapf)
+    elif watermodel == 'TIP3P':
+        print("solvatebox ion TIP3PBOX 13", file=leapf)
+    elif watermodel == 'SPC':
+        print("solvatebox ion SPCBOX 13", file=leapf)
+    elif watermodel == 'SPCE':
+        print("solvatebox ion SPCBOX 13", file=leapf)
+        print("loadamberparams frcmod.spce", file=leapf)
+    elif watermodel == 'TIP4P':
+        print("solvatebox ion TIP4PBOX 13", file=leapf)
+        print("loadamberparams frcmod.tip4p", file=leapf)
+    elif watermodel == 'TIP4PEW':
+        print("solvatebox ion TIP4PEWBOX 13", file=leapf)
+        print("loadamberparams frcmod.tip4pew", file=leapf)
+    elif watermodel == 'TIP5P':
+        print("solvatebox ion TIP5PBOX 13", file=leapf)
+        print("loadamberparams frcmod.tip5p", file=leapf)
+
+    print("loadamberparams %s.frcmod" %ion0.attype, file=leapf)  # VDW=0
+    print("loadamberparams %s.frcmod" %ion1.attype, file=leapf)  # VDW!=0
+
+    print("ion2 = copy ion", file=leapf)
+
+    # Only keep the ion without VDW or CHG
+    print("remove ion2 ion2.2", file=leapf)
+
+    # For sander, prmtop without VDW or CHG
+    print("saveamberparm ion2 %s_wat_s0.prmtop %s_wat_s0.inpcrd" %(ion0.element, ion0.element), file=leapf)
+
+    # For TI one-step method using pmemd, from the ion 0 to ion 1
+    print("saveamberparm ion %s_wat_pvc.prmtop %s_wat_pvc.inpcrd" %(ion0.element, ion0.element), file=leapf)
+
+    # VDW != 0
+    print("loadamberparams %s_vdw.frcmod" %ion0.attype, file=leapf)
+
+    # For sander, prmtop with VDW but not CHG
+    print("saveamberparm ion2 %s_wat_sv.prmtop %s_wat_sv.inpcrd" %(ion0.element, ion0.element), file=leapf)
+
+    # For TI two-step method using pmemd: from ion 0 (with VDW) to ion 1
+    print("saveamberparm ion %s_wat_pc.prmtop %s_wat_pc.inpcrd" %(ion0.element, ion0.element), file=leapf)
+
+    # For sander/MD, prmtop with VDW or CHG
+    print("remove ion ion.1", file=leapf)
+    print("saveamberparm ion %s_wat_svc.prmtop %s_wat_svc.inpcrd" %(ion0.element, ion0.element), file=leapf)
+
+    print("quit", file=leapf)
     leapf.close()
 
 def addc4(ion0, ion1, c4v):
 
+    # Only for sander
+    print("Add C4 parameters...")
+
     #Without charge and VDW
-    c4f0 = open(ion0.atname + '_c4.txt', 'w')
-    print >> c4f0, "%s %f" %(ion0.element+str(int(ion0.charge)), 0.0)
+    c4f0 = open('c4_0.txt', 'w')
+    print("%s %f" %(ion0.element+str(int(ion0.charge)), 0.0), file=c4f0)
     c4f0.close()
 
-    parmf = open('md0_addc4.in', 'w')
-    print >> parmf, "loadRestrt %s_wat_md.inpcrd" %ion0.atname
-    print >> parmf, "setOverwrite True"
-    print >> parmf, "add12_6_4 :1@%s c4file %s_c4.txt" %(ion0.atname, ion0.atname)
-    print >> parmf, "outparm %s_wat_md.prmtop %s_wat_md.inpcrd" %(ion0.atname, ion0.atname)
+    #Without VDW or charge
+    parmf = open('addc4_0.in', 'w')
+    print("loadRestrt %s_wat_s0.inpcrd" %ion0.element, file=parmf)
+    print("setOverwrite True", file=parmf)
+    print("add12_6_4 :1@%s c4file c4_0.txt" %ion0.atname, file=parmf)
+    print("outparm %s_wat_s0.prmtop %s_wat_s0.inpcrd" %(ion0.element, ion0.element), file=parmf)
     parmf.close()
-    os.system("parmed.py -i md0_addc4.in -p %s_wat_md.prmtop | tail -n 3" %ion0.atname)
 
-    #With VDW and witout charge
-    parmf = open('mdv_addc4.in', 'w')
-    print >> parmf, "loadRestrt %s_wat_vdw.inpcrd" %ion0.atname
-    print >> parmf, "setOverwrite True"
-    print >> parmf, "add12_6_4 :1@%s c4file %s_c4.txt" %(ion0.atname, ion0.atname)
-    print >> parmf, "outparm %s_wat_vdw.prmtop %s_wat_vdw.inpcrd" %(ion0.atname, ion0.atname)
+    os.system("parmed -i addc4_0.in -p %s_wat_s0.prmtop | tail -n 3" %ion0.element)
+
+    #With VDW but not charge
+    parmf = open('addc4_v.in', 'w')
+    print("loadRestrt %s_wat_sv.inpcrd" %ion0.element, file=parmf)
+    print("setOverwrite True", file=parmf)
+    print("add12_6_4 :1@%s c4file c4_0.txt" %(ion0.atname), file=parmf)
+    print("outparm %s_wat_sv.prmtop %s_wat_sv.inpcrd" %(ion0.element, ion0.element), file=parmf)
     parmf.close()
-    os.system("parmed.py -i mdv_addc4.in -p %s_wat_vdw.prmtop | tail -n 3" %ion0.atname)
+    os.system("parmed -i addc4_v.in -p %s_wat_sv.prmtop | tail -n 3" %ion0.element)
 
     #With Charge and VDW
-    c4f1 = open(ion1.element + '_c4.txt', 'w')
-    print >> c4f1, "%s %f" %(ion1.element+str(int(ion1.charge)), c4v)
+    c4f1 = open('c4_vc.txt', 'w')
+    print("%s %f" %(ion1.element+str(int(ion1.charge)), c4v), file=c4f1)
     c4f1.close()
 
-    parmf = open('md_addc4.in', 'w')
-    print >> parmf, "loadRestrt %s_wat_md.inpcrd" %ion1.element
-    print >> parmf, "setOverwrite True"
-    print >> parmf, "add12_6_4 :1@%s c4file %s_c4.txt" %(ion1.atname, ion1.element)
-    print >> parmf, "outparm %s_wat_md.prmtop %s_wat_md.inpcrd" %(ion1.element, ion1.element)
+    parmf = open('addc4_vc.in', 'w')
+    print("loadRestrt %s_wat_svc.inpcrd" %ion1.element, file=parmf)
+    print("setOverwrite True", file=parmf)
+    print("add12_6_4 :1@%s c4file c4_vc.txt" %(ion1.atname), file=parmf)
+    print("outparm %s_wat_svc.prmtop %s_wat_svc.inpcrd" %(ion1.element, ion1.element), file=parmf)
     parmf.close()
-    os.system("parmed.py -i md_addc4.in -p %s_wat_md.prmtop | tail -n 3" %ion1.element)
 
-def gene_topcrd(ion0, ion1, ifc4=0, c4v=0.0):
+    os.system("parmed -i addc4_vc.in -p %s_wat_svc.prmtop | tail -n 3" %ion1.element)
+
+def gene_topcrd(ion0, ion1, watermodel, ifc4=0, c4v=0.0):
 
     print("Generate the Topology and Coordinate Files...")
 
@@ -124,23 +165,26 @@ def gene_topcrd(ion0, ion1, ifc4=0, c4v=0.0):
 
     #pdb file
     pdbf = open('ION.pdb', 'w')
-    print >> pdbf, "HETATM 2032 %2s    %2s A   1       0.000   0.000   0.000  1.00  0.00" %(ion0.atname, ion0.resname)
-    print >> pdbf, "TER"
-    print >> pdbf, "HETATM 2032 %2s    %2s A   1       0.000   0.000   0.000  1.00  0.00" %(ion1.atname, ion1.resname)
+    print("HETATM 2032 %2s    %2s A   1       0.000   0.000   0.000  1.00  0.00" %(ion0.atname, ion0.resname), file=pdbf)
+    print("TER", file=pdbf)
+    print("HETATM 2032 %2s    %2s A   1       0.000   0.000   0.000  1.00  0.00" %(ion1.atname, ion1.resname), file=pdbf)
     pdbf.close()
 
     #frcmod file
     write_frcmod(ion0, ion0.attype + '.frcmod')
     write_frcmod(ion1, ion1.attype + '.frcmod')
+
     ion0.rmin = ion1.rmin
     ion0.ep = ion1.ep
+
     write_frcmod(ion0, ion0.attype + '_vdw.frcmod')
 
     #tleap file
-    write_leapin(ion0, ion1)
+    write_leapin(ion0, ion1, watermodel)
     os.system("tleap -s -f tleap.in > tleap.out")
 
     #add c4 parameters
     if ifc4 == 1:
         addc4(ion0, ion1, c4v)
+
 
