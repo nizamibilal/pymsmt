765f0697a3ee109c0d6f9a7bb873f189a214d522
Pengfei Li <pengfei@msu.edu>
2016-05-16 16:42:41 -0400

    Adjust the installation method to compatible with AmberTools14

---
+++ setup.sh
@@ -0,0 +1,12 @@
+for dir in api mcpb lib msmtmol ipmach
+do
+/bin/cp -LR $dir $1/
+done
+
+/bin/cp pymsmtexp.py $1/
+/bin/cp title.py $1/
+
+for prog in CartHess2FC.py espgen.py MCPB.py PdbSearcher.py
+do
+sed -e "s@\/usr\/bin\/env python@$2@g" < $prog > $1/$prog
+done
